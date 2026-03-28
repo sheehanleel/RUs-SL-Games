cf550850997b6da1c794b9e758d19ce516eb5e39
SheehanLatade-nmtafe <20128963@tafe.wa.edu.au>
2026-03-29 04:47:07 +0800

    feat:( Create function on filling up the buckets and getting index of player's UID )

--- app/player_hash_map.py
+++ app/player_hash_map.py
@@ -1,8 +1,25 @@
 from linked_list import LinkedList
+from player import Player
 
 class PlayerHashMap:
     size = 10
 
     def __init__(self):
+        #C reate a list to hold the 10 linkedlist
         self.buckets = []
-        self.buckets = [LinkedList() for _ in range(self.size)]
+
+        # Fill the buckets with linked list
+        for i in range(self.size):
+            self.buckets.append(LinkedList())
+    
+    def get_index(self, key):
+        # This method will take a player's UID and make it to a bucket index from 0 to 9
+
+        # Use the hash creator from Player Class
+        hash_value = Player.hash_uid(key)
+
+        # Shrink the number if it's to big to fit 0 to 9
+        index = hash_value % self.size
+
+        return index
+        
